2d92e8ae78d234831582a6e98be322ef275de72b
MakoaWork <santarinimakoa@gmail.com>
2018-10-04 11:06:21 -1000

    made a push before work

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -28,24 +29,18 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
         
-
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
 
-        import time
-        time.sleep(3)
+        time.sleep(2)
         
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
-
-
-##        table = self.browser.find_element_by_id('id_list_table')
-##        rows = table.find_elements_by_tag_name('tr')
-##        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
+    
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -56,21 +51,31 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
-##        table = self.browser.find_element_by_id('id_list_table')
-##        rows = table.find_elements_by_tag_name('tr')
-##        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-##        self.assertIn(
-##            '2: Use peacock feather to make a fly',
-##            [row.text for row in rows]
-##        )
 
-        # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some # explanatory text to that effect.
-        self.fail('Finish the test!')
-    
-        # She visits that URL - her to-do list is still there.
+        #Now a new user, Francis, comes along to the site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # Satisfied, she goes back to sleep
+        #francis visits the home page. There is no sign of Edit's list
+        self.browser.get(self.liver_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #Francis starts a new list by entering a new item. He is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #Francis gets his own unique URL
+        francis_list_url = self.browser.current.url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #Again, ther is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #satisfied, they both go back to sleep
         
-##if __name__ == '__main__':
-##    unittest.main(warnings='ignore')
